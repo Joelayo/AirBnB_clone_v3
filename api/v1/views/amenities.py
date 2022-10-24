1f1473feb04c713f0ad9e6297d7baeb02aff5011
Joelayo <joetech.ooj@gmail.com>
2022-10-24 20:41:33 +0100

    Added Restapi methods to handle Amenities objects

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -2,6 +2,7 @@
 """This module implements a view for amenities objects"""
 from api.v1.views import app_views
 from flask import jsonify, abort, request
+from models import amenity
 from models.amenity import Amenity
 import models
 
@@ -27,12 +28,12 @@ def show_amenity(amenity_id):
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
 def delete_amenity(amenity_id):
     """ Deletes an amenity object in db storage """
-    state = models.storage.get("Amenity", amenity_id)
-    if state is None:
-        abort(404)
-    state.delete()
-    models.storage.save()
-    return jsonify({})
+    amenity = models.storage.get("Amenity", amenity_id)
+    if amenity:
+        amenity.delete()
+        models.storage.save()
+        return jsonify({})
+    abort(404)
 
 
 @app_views.route('/amenities/', strict_slashes=False, methods=['POST'])
@@ -43,21 +44,21 @@ def create_amenity():
     if 'name' not in request.json:
         return jsonify({"error": "Missing name"}), 400
     content = request.get_json()
-    state = Amenity(**content)
-    state.save()
-    return jsonify(state.to_dict()), 201
+    amenity = Amenity(**content)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 201
 
 
 @app_views.route('/amenities/<amenity_id>', strict_slashes=False, methods=['PUT'])
 def update_amenity(amenity_id):
     ''' Upddate the amenity object with the given amenity_id '''
-    state = models.storage.get("Amenity", amenity_id)
-    if state is None:
+    amenity = models.storage.get("Amenity", amenity_id)
+    if amenity is None:
         abort(404)
     if not request.get_json():
         return jsonify({'error': 'Not a JSON'}), 400
     for key, val in request.get_json().items():
         if key != 'id' and key != 'created_at' and key != 'updated_at':
-            setattr(state, key, val)
-    state.save()
-    return jsonify(state.to_dict()), 200
+            setattr(amenity, key, val)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 200
