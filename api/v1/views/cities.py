efdfb8f993484446af89f66bbfb1c0b457fe596f
Joelayo <joetech.ooj@gmail.com>
2022-10-24 21:21:14 +0100

    Added Restapi methods to handle Cities objects

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,7 +6,10 @@ from models.city import City
 import models
 
 
-@app_views.route('/states/<state_id>/cities', strict_slashes=False, methods=['GET'])
+@app_views.route(
+    '/states/<state_id>/cities',
+    strict_slashes=False,
+    methods=['GET'])
 def retrieve_city_objs(state_id):
     """ Shows all city objects """
     state = models.storage.get("State", state_id)
